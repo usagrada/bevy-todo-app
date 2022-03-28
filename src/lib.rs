f4adaa33bc47d1b10079a24ba70895edc44c23ae
usagrada <usagrada.kind@gmail.com>
2022-03-28 14:23:35 +0900

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+use font_kit::{
+    error::SelectionError,
+    family_name::FamilyName,
+    handle::Handle,
+    properties::{Properties, Style},
+    source::SystemSource,
+};
+
+/// Returns the default system font.
+pub fn default_system_font() -> Result<Handle, SelectionError> {
+    let source = SystemSource::new();
+    let default_fonts = &[
+        FamilyName::Title("arial".to_string()),
+        FamilyName::SansSerif,
+        FamilyName::Monospace,
+        FamilyName::Fantasy,
+    ];
+    source.select_best_match(
+        default_fonts,
+        Properties::new().style(Style::Normal),
+    )
+}
