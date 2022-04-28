e4d2986a9354bf8606b16090b1d52452c51e99a2
usagrada <usagrada.kind@gmail.com>
2022-04-29 02:28:48 +0900

    yew で for 文にonclickを渡せるように

--- web-old/src/pages.rs
+++ web-old/src/pages.rs
@@ -104,15 +104,13 @@ fn side_bar() -> Html {
 
     let update_channel = {
         let channels_handler = channels_handler.clone();
-        {
-            // let chs = channels.clone();
-            // Callback::from(move |new_channel: &str| {
-            //     channels_handler.set(Channels {
-            //         select: new_channel.to_string(),
-            //         channels: chs.clone(),
-            //     });
-            // })
-        }
+        let chs = channels.clone();
+        Callback::from(move |new_channel: String| {
+            channels_handler.set(Channels {
+                select: new_channel.to_string(),
+                channels: chs.clone(),
+            });
+        })
     };
 
     html!(
@@ -122,16 +120,15 @@ fn side_bar() -> Html {
             </div>
             {
                 for channels.iter().map(|ch: &String| {
-                    // let update_channel = update_channel.clone();
                     let onclick = {
-                        // let ch = ch.clone();
-                        // Callback::from(move |_| {
-                        //     update_channel.emit(&ch);
-                        //     // move |_| update_channel.emit(&ch);
-                        // })
+                        let update_channel = update_channel.clone();
+                        let ch = ch.clone();
+                        Callback::from(move |_| {
+                            update_channel.emit(ch.clone());
+                        })
                     };
                     html! {
-                        <div class="task" onclick={onclick.clone()}>
+                        <div class="task" onclick={onclick}>
                             {&ch}
                         </div>
                     }
